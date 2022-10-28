58455f0f88a30163690777ffae2671b14bde1ae0
StarFleet1334 <86927329+StarFleet1334@users.noreply.github.com>
2022-10-28 17:05:39 +0400

    HomePage Login

--- src/main/java/com/qaprosoft/carina/demo/gui/testPages/HomePage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/testPages/HomePage.java
@@ -1,4 +1,21 @@
 package com.qaprosoft.carina.demo.gui.testPages;
 
-public class HomePage {
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends AbstractPage {
+
+    @FindBy(xpath = "//*[@id=\"__aer_root__\"]/div/div[3]/div/div/div/div/div/div[2]/div/nav/ul/li[1]/div")
+    private ExtendedWebElement homePageLoginBtn;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        setPageURL("https://aliexpress.ru/");
+    }
+
+    public boolean isLoginPresent() {
+        return homePageLoginBtn.isElementPresent();
+    }
 }
--- src/test/java/com/qaprosoft/carina/demo/tests/HomePageTest.java
+++ src/test/java/com/qaprosoft/carina/demo/tests/HomePageTest.java
@@ -1,4 +1,18 @@
 package com.qaprosoft.carina.demo.tests;
 
-public class HomePageTest {
+import com.qaprosoft.carina.core.foundation.AbstractTest;
+import com.qaprosoft.carina.demo.gui.testPages.HomePage;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class HomePageTest extends AbstractTest {
+
+
+    @Test
+    public void checkHomePageLogin() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        assertTrue(homePage.isLoginPresent(),"Not Present");
+    }
 }
